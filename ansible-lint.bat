f6f531e5998f8b1d04e96bd81855ee86f0ce6fe8
Kenneth Dahle <kenneth@k96.no>
2023-10-29 19:55:40 +0100

    Create ansible-lint.bat

---
+++ ansible-lint.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+SETLOCAL
+
+CALL :lastarg xxx %*
+CALL :skiplastarg yyy %*
+wsl --shell-type login ansible-lint %yyy% $(wslpath -a "%XXX%")
+GOTO :EOF
+
+:: Return all but last arg in variable given in %1
+:skiplastarg returnvar args ...
+    SETLOCAL
+        SET $return=%1
+        SET SKIP_LAST_ARG=
+        SHIFT
+    :skiplastarg_2
+        IF NOT "%~2"=="" SET "SKIP_LAST_ARG=%SKIP_LAST_ARG% %1"
+        SHIFT
+        IF NOT "%~1"=="" GOTO skiplastarg_2
+    ENDLOCAL&CALL SET "%$return%=%SKIP_LAST_ARG:~1%"
+GOTO :EOF
+
+:: Return last arg in variable given in %1
+:lastarg returnvar args ...
+    SETLOCAL
+      SET $return=%1
+      SET LAST_ARG=
+  SHIFT
+    :LASTARG_2
+      SET "LAST_ARG=%1"
+      SHIFT
+      IF NOT "%~1"=="" GOTO lastarg_2
+    ENDLOCAL&call SET %$return%=%LAST_ARG%
+GOTO :EOF
